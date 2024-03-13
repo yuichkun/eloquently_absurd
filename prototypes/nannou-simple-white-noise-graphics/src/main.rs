28f55caabbcfacd52348fdb5528cd29126daddfa
yuichkun <yuichkun@gmail.com>
2024-03-13 18:35:25 +0900

    Update model initialization and add update function

--- prototypes/nannou-simple-white-noise-graphics/src/main.rs
+++ prototypes/nannou-simple-white-noise-graphics/src/main.rs
@@ -2,15 +2,16 @@ use nannou::image;
 use nannou::prelude::*;
 
 fn main() {
-    nannou::app(model).run();
+    nannou::app(model).update(update).run();
 }
 
 struct Model {
     texture: wgpu::Texture,
+    b: u8,
 }
 
 fn model(app: &App) -> Model {
-    app.new_window().view(view).build().unwrap();
+    app.new_window().size(300, 300).view(view).build().unwrap();
     let window = app.main_window();
     let win = app.window_rect();
     let width = win.w() as u32;
@@ -21,7 +22,7 @@ fn model(app: &App) -> Model {
         .format(wgpu::TextureFormat::Rgba8Unorm)
         .usage(wgpu::TextureUsages::COPY_DST | wgpu::TextureUsages::TEXTURE_BINDING)
         .build(window.device());
-    Model { texture }
+    Model { texture, b: 0 }
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
@@ -35,7 +36,7 @@ fn view(app: &App, model: &Model, frame: Frame) {
         let normalized_y = y as f32 / height as f32;
         let r: u8 = (normalized_x * 255.0).round() as u8;
         let g: u8 = (normalized_y * 255.0).round() as u8;
-        nannou::image::Rgba([r, g, 0, std::u8::MAX])
+        nannou::image::Rgba([r, g, model.b, std::u8::MAX])
     });
     let flat_samples = image.as_flat_samples();
     model.texture.upload_data(
@@ -46,4 +47,13 @@ fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.texture(&model.texture);
     draw.to_frame(app, &frame).unwrap();
+}
+
+fn update(_app: &App, model: &mut Model, _update: Update) {
+    if model.b == std::u8::MAX {
+        model.b = 0;
+    } else {
+        model.b += 3;
+    }
+    println!("update b: {}", model.b);
 }
