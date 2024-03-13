24d7ef685d28b04dcf6a57c345a269d84f6618a3
yuichkun <yuichkun@gmail.com>
2024-03-13 18:31:24 +0900

    Refactor view function and add Model struct

--- prototypes/nannou-simple-white-noise-graphics/src/main.rs
+++ prototypes/nannou-simple-white-noise-graphics/src/main.rs
@@ -2,14 +2,17 @@ use nannou::image;
 use nannou::prelude::*;
 
 fn main() {
-    nannou::sketch(view_2).run();
+    nannou::app(model).run();
 }
 
-fn view_2(app: &App, frame: Frame) {
-    frame.clear(BLACK);
+struct Model {
+    texture: wgpu::Texture,
+}
 
+fn model(app: &App) -> Model {
+    app.new_window().view(view).build().unwrap();
     let window = app.main_window();
-    let win = window.rect();
+    let win = app.window_rect();
     let width = win.w() as u32;
     let height = win.h() as u32;
 
@@ -18,23 +21,29 @@ fn view_2(app: &App, frame: Frame) {
         .format(wgpu::TextureFormat::Rgba8Unorm)
         .usage(wgpu::TextureUsages::COPY_DST | wgpu::TextureUsages::TEXTURE_BINDING)
         .build(window.device());
+    Model { texture }
+}
+
+fn view(app: &App, model: &Model, frame: Frame) {
+    frame.clear(BLACK);
+    let win = app.window_rect();
+    let width = win.w() as u32;
+    let height = win.h() as u32;
+
     let image = image::ImageBuffer::from_fn(width, height, |x, y| {
         let normalized_x = x as f32 / width as f32;
         let normalized_y = y as f32 / height as f32;
         let r: u8 = (normalized_x * 255.0).round() as u8;
         let g: u8 = (normalized_y * 255.0).round() as u8;
-        return nannou::image::Rgba([r, g, 0, std::u8::MAX]);
+        nannou::image::Rgba([r, g, 0, std::u8::MAX])
     });
     let flat_samples = image.as_flat_samples();
-    texture.upload_data(
+    model.texture.upload_data(
         app.main_window().device(),
-        &mut *frame.command_encoder(),
+        &mut frame.command_encoder(),
         &flat_samples.as_slice(),
     );
-
     let draw = app.draw();
-    draw.texture(&texture);
-
-    // Write to the window frame.
+    draw.texture(&model.texture);
     draw.to_frame(app, &frame).unwrap();
 }
