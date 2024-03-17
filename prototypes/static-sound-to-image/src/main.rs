d12f7a9e86d4d4bb22fd224202ae9dfc1ebc279e
yuichkun <yuichkun@gmail.com>
2024-03-17 18:40:53 +0900

    Update sound-to-image prototype with sample drawing optimization

--- prototypes/static-sound-to-image/src/main.rs
+++ prototypes/static-sound-to-image/src/main.rs
@@ -7,13 +7,15 @@ use utils::DrawExt;
 const DOT_SIZE: f32 = 1.0;
 const IMAGE_WIDTH: f32 = 720.0;
 const IMAGE_HEIGHT: f32 = 615.0;
+const SAMPLES_TO_DRAW_AT_ONE_FRAME: usize = 1000;
 
 fn main() {
-    nannou::app(model).run();
+    nannou::app(model).update(update).run();
 }
 
 struct AppModel {
     buffer: Vec<i16>,
+    offset: usize,
 }
 
 fn model(app: &App) -> AppModel {
@@ -31,16 +33,22 @@ fn model(app: &App) -> AppModel {
 
     let buffer: Vec<i16> = reader.samples::<i16>().filter_map(Result::ok).collect();
 
-    AppModel { buffer }
+    AppModel { buffer, offset: 0 }
 }
 
 fn view(app: &App, model: &AppModel, frame: Frame) {
     let draw = app.draw();
-    frame.clear(BLACK);
+    // frame.clear(BLACK);
     let win = app.window_rect();
     draw.point(win.w() / 2.0, win.h() / 2.0, gray(1.0));
 
-    for (index, sample) in model.buffer.iter().enumerate() {
+    for (index, sample) in model
+        .buffer
+        .iter()
+        .enumerate()
+        .skip(model.offset)
+        .take(SAMPLES_TO_DRAW_AT_ONE_FRAME)
+    {
         let (x, y) = index_to_xy(index);
         let color = gray(map_range(*sample, i16::MIN, i16::MAX, 0.0, 1.0));
         draw.point(x, y, color);
@@ -48,6 +56,10 @@ fn view(app: &App, model: &AppModel, frame: Frame) {
 
     draw.to_frame(app, &frame).unwrap();
     println!("FPS: {}", app.fps());
+}
+fn update(_app: &App, model: &mut AppModel, _update: Update) {
+    // This is where you can safely modify your model
+    model.offset = (model.offset + SAMPLES_TO_DRAW_AT_ONE_FRAME) % model.buffer.len();
 }
 
 fn index_to_xy(index: usize) -> (f32, f32) {
