6de39209197a0378c5548f5a7d9ed4fe9b4a4db3
yuichkun <yuichkun@gmail.com>
2024-03-27 04:12:22 +0900

    add uniforms

--- prototypes/simple-shader/src/main.rs
+++ prototypes/simple-shader/src/main.rs
@@ -8,10 +8,18 @@ struct Model {
     bind_group: BindGroup,
     render_pipeline: RenderPipeline,
     vertex_buffer: Buffer,
+    uniforms: Uniforms,
+    uniform_buffer: Buffer,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug)]
+pub struct Uniforms {
+    time: f32,
 }
 
 fn main() {
-    nannou::app(model).run();
+    nannou::app(model).update(update).run();
 }
 
 fn model(app: &App) -> Model {
@@ -24,29 +32,43 @@ fn model(app: &App) -> Model {
     let fs_desc = include_wgsl!("shaders/fs.wgsl");
 
     let sample_count = window.msaa_samples();
+
+    let uniforms = Uniforms { time: 0.0 };
+
     let SetupRenderPipelineOutput {
         bind_group,
         render_pipeline,
         vertex_buffer,
+        uniform_buffer,
     } = setup_render_pipeline(SetupRenderPipelineParams {
         device,
         vs_desc,
         fs_desc,
         sample_count,
+        uniforms: &uniforms,
     });
 
     Model {
         bind_group,
         vertex_buffer,
         render_pipeline,
+        uniforms,
+        uniform_buffer,
     }
+}
+
+fn update(app: &App, model: &mut Model, _update: Update) {
+    model.uniforms.time = app.time;
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
     render_shaders(
+        model,
         &model.bind_group,
         &model.render_pipeline,
         &model.vertex_buffer,
         &frame,
+        app.main_window().device(),
+        &model.uniforms,
     );
 }
